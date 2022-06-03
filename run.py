e014624f29a87e1cf6c5f018ed55762388368990
Alexa Hendry <alexahendry88@gmail.com>
2022-06-03 15:25:27 +0000

    Create assign_wand function to assign randomly chosen wand to player

--- run.py
+++ run.py
@@ -2,26 +2,55 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
+
+
 def choose_play_game():
     """
     Asks the user if they want to play the game or not
     """
-    play_game_options = input("Ready to face up to \
-        the challenge? y/n \n").lower()
+    play_game_options = input("Ready to face up to the challenge? (y/n) \n")
     if play_game_options == "y":
-        play_game()
+        #play_game()
+        print("Play game")
     elif play_game_options == "n":
-        exit_game()
+        #exit_game()
+        print("Exit game")
     else:
-        print("Please choose either y or n, in lowercase.")
+        print("Please choose either y or n.")
 
 
 def choose_player_name():
     """
-    Asks the user to choose their witch or wizard name
+    Asks the player to choose their witch or wizard name
     """
     player_name = input("Choose your witch or wizard name: \n")
     print(f"Welcome, {player_name}!")
+
+
+def assign_wand():
+    """
+    Assigns the player one of three wand options
+    """
+    wand_options = random.choice([
+        "Harry Potter's Wand",
+        "James Potter's Wand",
+        "Lily Potter's Wand"])
+    print("Garrick Ollivander looks you up and down, studying you carefully.")
+    print(f"Test, {wand_options} will suit you nicely!")
+    #print(f"{player_name}, {wand_options} will suit you nicely!")
+
+
+#def play_game():
+#"""
+#Starts the game
+#"""
+
+
+#def exit_game():
+#"""
+#Exits the game
+#"""
 
 
 def main():
@@ -30,6 +59,9 @@ def main():
     """
     choose_player_name()
     choose_play_game()
+    assign_wand()
+    #play_game()
+    #exit_game()
 
 
 main()
