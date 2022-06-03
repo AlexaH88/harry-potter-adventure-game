802c62fb08f924eecae0eb11961707ef415890a4
Alexa Hendry <alexahendry88@gmail.com>
2022-06-03 21:13:21 +0000

    Add extra line spacing, create wand_request function, rephrase assing-wand function

--- run.py
+++ run.py
@@ -12,14 +12,13 @@ def game_intro():
     """
     print("Welcome to the Harry Potter Adventure Game!")
     print("\u26A1")
-    print("Strange things have been happening at Hogwarts School of Witchcraft \
-        and Wizardry...")
+    print("Strange things have been happening at Hogwarts lately...")
     print("\U0001F3F0")
     print("And no one seems to be able to work out how to stop them...")
     print("Could you be the witch or wizard we've been looking for?")
     print("\U0001F9D9\u200D\u2642\uFE0F")
     print("Please help, the Wizarding World needs you!")
-    print("\U0001F30D")
+    print("\U0001F30D \n")
 
 
 def choose_play_game():
@@ -29,14 +28,14 @@ def choose_play_game():
     play_game_options = input("Ready to face up to the challenge? (y/n) \n")
     if play_game_options == "y":
         print("Great choice, let the adventure begin!")
-        print("\U0001F642")
+        print("\U0001F642 \n")
     elif play_game_options == "n":
         print("That's a shame, maybe next time!")
-        print("\U0001F641")
+        print("\U0001F641 \n")
         exit_game()
     else:
         print("Please choose either y or n.")
-        print("\U0001F610")
+        print("\U0001F610 \n")
         choose_play_game()
 
 
@@ -46,6 +45,7 @@ def choose_player_name():
     """
     player_name = input("Choose your witch or wizard name: \n")
     print(f"Welcome, {player_name}!")
+    print("\U0001F9D9\u200D\u2642\uFE0F \n")
 
 
 def wand_backstory():
@@ -53,15 +53,36 @@ def wand_backstory():
     Prints backstory on Ollivander and player wand selection
     """
     print("Now, first things first.")
-    print("In order to beat Voldemort you're going to need a wand!")
-    print("So, let's ask our favourite wandmaker, \
-        Garrick Ollivander to assign you one!")
+    print("In order to save Hogwarts you're going to need a wand!")
+    print("\U0001FA84")
+    print("Let's go and see our favourite wandmaker, Garrick Ollivander! \n")
+
+
+def wand_request():
+    """
+    Asks the player to request a wand
+    """
+    ask_for_wand = input(
+        "Ollivander: Welcome to my shop, are you in need of a wand? (y/n) \n")
+    if ask_for_wand == "y":
+        print("Lovely, let's see what we can find for you!")
+        print("\U0001F642 \n")
+    elif ask_for_wand == "n":
+        print("Fine, but you won't get far without one!")
+        print("\U0001F641 \n")
+        exit_game()
+    else:
+        print("Please choose either y or n.")
+        print("\U0001F610 \n")
+        wand_request()
 
 
 def assign_wand():
     """
     Randomly assigns the player one of three wand options
     """
+    print("Ollivander looks you up and down, studying you carefully...")
+
     wand_options = [
         "Harry Potter's Wand",
         "Albus Dumbledore's Wand",
@@ -69,19 +90,18 @@ def assign_wand():
 
     random_wand_options = random.choice(wand_options)
 
-    print("Ollivander looks you up and down, studying you carefully.")
     print(f"{random_wand_options} will suit you nicely!")
     #print(f"{player_name}, {wand_options} will suit you nicely!")
 
     if "Harry Potter's Wand" in random_wand_options:
-        print(f"{wand_options[0]} is 11 inches long, made with holly wood, \
+        print("This wand is 11 inches long, made with holly wood, \
             has a Phoenix feather core, and is nice and supple.")
     elif "Albus Dumbledore's Wand" in random_wand_options:
-        print(f"{wand_options[1]} is 15 inches long, made with elder wood, \
+        print("This wand is 15 inches long, made with elder wood, \
             has a Thestral tail hair core, and is the most powerful wand ever \
             to exist!")
     elif "Rubeus Hagrid's Wand" in random_wand_options:
-        print(f"{wand_options[2]} is 16 inches long, made with oak wood, \
+        print("This wand is 16 inches long, made with oak wood, \
             has an unknown core, and is rather bendy.")
 
 
@@ -100,6 +120,7 @@ def main():
     choose_play_game()
     choose_player_name()
     wand_backstory()
+    wand_request()
     assign_wand()
 
 
