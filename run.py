788a03b16f6f3d85be2bb089ffc3afc14d3b13ec
Alexa Hendry <alexahendry88@gmail.com>
2022-06-03 13:59:32 +0000

    Create choose_player_name function to get player name from user

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def choose_player_name():
+    """
+    Player chooses witch or wizard name
+    """
+    player_name = input("Choose your witch or wizard name:\n ")
+    print(f"Welcome, {player_name}!")
+
+
+choose_player_name()
