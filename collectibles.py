a1f225376c870ecc14261553e45d06667db1ec01
Alexa Hendry <alexahendry88@gmail.com>
2022-06-17 12:37:18 +0000

    Improve spacing in wand description

--- collectibles.py
+++ collectibles.py
@@ -32,9 +32,9 @@ class Wand:
         Describes the wand
         """
         return (
-            f"'This wand is {self.length} long, \n"
+            f"'This wand is {self.length} long, "
             f"is made with {self.wood} wood, \n"
-            f"has {self.core} core, \n"
+            f"has {self.core} core, "
             f"and is {self.characteristic}!'"
         )
 
